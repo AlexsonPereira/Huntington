11e197307c682a3bfef610ec4f2e906f02bfe60d
alexson pereira <alexspnpe@gmail.com>
2024-12-20 08:38:07 -0300

    first

---
+++ assets/JS/index.js
@@ -0,0 +1,24 @@
+function showSubMenu(){
+    const submenu = document.getElementById("submenu-mobile")
+    if(submenu.classList.contains("flex")){
+        submenu.classList.remove("flex")
+        submenu.classList.add("hidden")
+        setTimeout(() => {
+           submenu.classList.remove('opacity-100');
+           submenu.classList.add('opacity-0');
+           submenu.classList.add('-translate-x-full');
+           submenu.classList.remove('translate-x-0');
+
+        }, 100);
+    }else{
+        submenu.classList.add("flex")
+        submenu.classList.remove("hidden")
+        setTimeout(() => {
+            submenu.classList.remove('opacity-0');
+            submenu.classList.add('opacity-100');
+            submenu.classList.remove('-translate-x-full')
+            submenu.classList.add('translate-x-0') 
+        }, 100);
+    }
+    
+}
